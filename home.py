62d9826272ff7ae427352fe30048c8bad70bf036
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-12 15:21:15 +0100

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ with st.expander('Laatvlieger'):
   Laatvlieger_date = st.date_input("Period", value=(datetime.date(year, 4, 15), datetime.date(year, 5, 15)), 
                                    min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, 
                                    kwargs=None,format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-  Laatvlieger_days = st.number_input("How many days", min_value=0, max_value=2, value="max", step=1, format=None, key=None, 
+  Laatvlieger_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key=None, 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
 
@@ -17,7 +17,7 @@ with st.expander('Kraamverblif'):
   Kraamverblif_date = st.date_input("Period", value=(datetime.date(year, 5, 15), datetime.date(year, 7, 15)), 
                                    min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, 
                                    kwargs=None,format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-  Kraamverblif_days = st.number_input("How many days", min_value=0, max_value=3, value="max", step=1, format=None, key=None, 
+  Kraamverblif_days = st.number_input("How many days", min_value=0, max_value=3, value=3, step=1, format=None, key=None, 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
 
@@ -25,7 +25,7 @@ with st.expander('Winterverblijf'):
   Winterverblijf_date = st.date_input("Period", value=(datetime.date(year, 4, 15), datetime.date(year, 5, 15)), 
                                    min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, 
                                    kwargs=None,format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-  Winterverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value="max", step=1, format=None, key=None, 
+  Winterverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key=None, 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
 
@@ -33,7 +33,7 @@ with st.expander('Paarverblijf'):
   Paarverblijf_date = st.date_input("Period", value=(datetime.date(year, 4, 15), datetime.date(year, 5, 15)), 
                                    min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, 
                                    kwargs=None,format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-  Paarverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value="max", step=1, format=None, key=None, 
+  Paarverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key=None, 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
 
@@ -41,7 +41,7 @@ with st.expander('Tweekleirige'):
   Tweekleirige_date = st.date_input("Period", value=(datetime.date(year, 4, 15), datetime.date(year, 5, 15)), 
                                    min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, 
                                    kwargs=None,format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-  Tweekleirige_days = st.number_input("How many days", min_value=0, max_value=2, value="max", step=1, format=None, key=None, 
+  Tweekleirige_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key=None, 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
 
