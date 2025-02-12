8cbd03d0ef5f266a7e72f079accf076c6eb862a4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-12 16:37:51 +0100

    Update home.py

--- home.py
+++ home.py
@@ -12,6 +12,9 @@ with st.expander('Laatvlieger'):
   Laatvlieger_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key="Laatvlieger_days", 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
+  number_areas_Laatvlieger = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas_Laatvlieger", 
+                                   help=None, on_change=None, args=None, kwargs=None,
+                                   placeholder=None, disabled=False, label_visibility="visible")
 
 with st.expander('Kraamverblif'):
   Kraamverblif_date = st.date_input("Period", value=(datetime.date(year, 5, 15), datetime.date(year, 7, 15)), 
@@ -20,6 +23,9 @@ with st.expander('Kraamverblif'):
   Kraamverblif_days = st.number_input("How many days", min_value=0, max_value=3, value=3, step=1, format=None, key="Kraamverblif_days", 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
+  number_areas_Kraamverblif = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas_Kraamverblif", 
+                                   help=None, on_change=None, args=None, kwargs=None,
+                                   placeholder=None, disabled=False, label_visibility="visible")
 
 with st.expander('Winterverblijf'):
   Winterverblijf_date = st.date_input("Period", value=(datetime.date(year, 4, 15), datetime.date(year, 5, 15)), 
@@ -28,6 +34,9 @@ with st.expander('Winterverblijf'):
   Winterverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key="Winterverblijf_days", 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
+  number_areas_Winterverblijf = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas_Winterverblijf", 
+                                   help=None, on_change=None, args=None, kwargs=None,
+                                   placeholder=None, disabled=False, label_visibility="visible")
 
 with st.expander('Paarverblijf'):
   Paarverblijf_date = st.date_input("Period", value=(datetime.date(year, 9, 1), datetime.date(year, 10, 1)), 
@@ -36,6 +45,9 @@ with st.expander('Paarverblijf'):
   Paarverblijf_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key="Paarverblijf_days", 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
+  number_areas_Paarverblijf = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas_Paarverblijf", 
+                                   help=None, on_change=None, args=None, kwargs=None,
+                                   placeholder=None, disabled=False, label_visibility="visible")
 
 with st.expander('Tweekleirige'):
   Tweekleirige_date = st.date_input("Period", value=(datetime.date(year, 10, 1), datetime.date(year, 11, 1)), 
@@ -44,20 +56,20 @@ with st.expander('Tweekleirige'):
   Tweekleirige_days = st.number_input("How many days", min_value=0, max_value=2, value=2, step=1, format=None, key="Tweekleirige_days", 
                                      help=None, on_change=None, args=None, kwargs=None,
                                      placeholder=None, disabled=False, label_visibility="visible")
-
-
-number_areas = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas", 
+  number_areas_Tweekleirige = st.number_input("How many areas", min_value=0, max_value=None, value="min", step=1, format=None, key="number_areas_Tweekleirige", 
                                    help=None, on_change=None, args=None, kwargs=None,
                                    placeholder=None, disabled=False, label_visibility="visible")
 
+
+
 days_Laatvlieger =  (Laatvlieger_date[1] - Laatvlieger_date[0]).days
-days_work_Laatvlieger = number_areas*Laatvlieger_days
+days_work_Laatvlieger = number_areas_Laatvlieger*Laatvlieger_days
 days_off = days_Laatvlieger - days_work_Laatvlieger
 days_off_percent = round((days_off*100)/days_Laatvlieger)
 st.write(f"If you cover {number_areas} areas during the Laatvleger period you will work {days_work_Laatvlieger} days, and {days_off} days off which is the {days_off_percent}% of the total number in that peroid")
   
 days_Kraamverblif =  (Kraamverblif_date[1] - Kraamverblif_date[0]).days
-days_work_Kraamverblif = number_areas*Kraamverblif_days
+days_work_Kraamverblif = number_areas_Kraamverblif*Kraamverblif_days
 days_off = days_Kraamverblif - days_work_Kraamverblif
 days_off_percent = round((days_off*100)/days_Kraamverblif)
 st.write(f"If you cover {number_areas} areas during the Kraamverblif period you will work {days_work_Kraamverblif} days, and {days_off} days off which is the {days_off_percent}% of the total number in that peroid")  
